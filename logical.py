680a8e8c36739f83b24363a9c450b286a9d9cf66
Navya-Dasari <125664719+Navya-Dasari@users.noreply.github.com>
2023-10-11 18:41:45 +0530

    Create logical.py

---
+++ logical.py
@@ -0,0 +1,10 @@
+sno=int(input("Enter rollno:"))
+sname=input("Enter Name:")
+Group=input("Enter Group name:")
+s1=int(input("Enter Java Marks:"))
+s2=int(input("Enter OS Marks:"))
+s3=int(input("Enter AFM Marks:"))
+if(s1>40 and s2>40 and s3>40):
+    print("Pass")
+else:
+    print("Fail")
