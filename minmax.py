ac9a470affc8fadf214fb5b9be0b13d9ad47f9f6
Navya-Dasari <125664719+Navya-Dasari@users.noreply.github.com>
2023-11-16 08:19:09 +0530

    Create minmax.py

---
+++ minmax.py
@@ -0,0 +1,14 @@
+m=[]
+n=int(input())
+for i in range(n):
+    x=int(input())
+    m.append(x)
+min=m[0]
+max=m[0]
+for i in range(n):
+    if m[i]>max:
+        max=m[i]
+    elif m[i]<min:
+        min=m[i]
+print(min)
+print(max)
